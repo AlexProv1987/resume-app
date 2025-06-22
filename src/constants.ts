d8a8d3c22d293d7462c1eb841310e10612e08c9d
AlexProv1987 <alex.provenzano87@gmail.com>
2025-06-22 12:28:42 -0500

    im a big ol dummy dumb

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,1 @@
-export const applicant:string = process.env.APPLICANT ? process.env.APPLICANT : '2ae5a1270a454e498084d4fdd5e503ea';
+export const applicant:string = process.env.REACT_APP_APPLICANT ? process.env.REACT_APP_APPLICANT : '2ae5a1270a454e498084d4fdd5e503ea';
