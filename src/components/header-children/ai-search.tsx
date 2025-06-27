45370fe240c845050d0e69ee66675d0916f0d256
AlexProv1987 <alex.provenzano87@gmail.com>
2025-06-27 13:31:31 -0500

    Save Question Answer Feature

--- src/components/header-children/ai-search.tsx
+++ src/components/header-children/ai-search.tsx
@@ -1,15 +1,16 @@
 import { useState } from "react"
-import { Form, Container, Row, Col, Modal, Button} from "react-bootstrap"
+import { Form, Container, Row, Col, Modal, Button } from "react-bootstrap"
 import { CenteredSpinner } from "../common/centered-spinner"
 import { axiosBaseURL } from "../../http"
 import { DownloadResume } from "./download_resume"
 import { BouncingDotsLoader } from "../common/bouncing-loader"
-import { applicant } from "../../constants"
+
 interface SearchCompProps {
     applicant_id: string,
     applicant_name_first: string,
     banner_img: string,
 }
+
 //we need to check device for width needs to be 100% on mobile
 export const SearchComponent = (props: SearchCompProps) => {
 
@@ -18,8 +19,10 @@ export const SearchComponent = (props: SearchCompProps) => {
     const [showError, setShowError] = useState(false)
     const [answer, setAnswer] = useState<string>('')
 
+    const inputMinLength = 25
+
     const submitQuestion = () => {
-        if (inputVal.length < 25) {
+        if (inputVal.length < inputMinLength) {
             setShowError(true);
             return;
         }
@@ -27,10 +30,15 @@ export const SearchComponent = (props: SearchCompProps) => {
         setShow(true);
 
         axiosBaseURL.post('question/ask/', {
-            applicant: applicant,
+            applicant: props.applicant_id,
             question: inputVal,
         }).then(function (response) {
             setAnswer(response.data.answer)
+            axiosBaseURL.post('feedback/create/llmresponse', {
+                applicant_reltn:props.applicant_id,
+                question_text:inputVal,
+                question_answer:response.data.answer
+            })
         }).catch(function (error) {
             setAnswer('It appears our overlord is having issues today. Please try again later.')
         }).finally(function () {
@@ -51,49 +59,51 @@ export const SearchComponent = (props: SearchCompProps) => {
                 justifyContent: 'center',
                 alignItems: 'center',
             }}>
+
             {props.applicant_name_first !== 'undefined' ?
                 <>
                     <DownloadResume applicant={props.applicant_id} />
                     <Row className="w-100 justify-content-center px-3">
                         <Col xs={12} sm={10} md={8} lg={6}>
-                                <Form.Group controlId="questionInput">
-                                    <Form.Control
-                                        className="ask-input"
-                                        type="text"
-                                        value={inputVal}
-                                        onChange={(e) => {
-                                            setInputVal(e.target.value);
-                                            if (showError && e.target.value.length >= 25) {
-                                                setShowError(false);
-                                            }
-                                        }}
-                                        placeholder={`Ask about ${props.applicant_name_first}’s relevance to the role...`}
-                                        isInvalid={showError}
-                                        maxLength={1000}
-                                        onKeyDown={(e) => {
-                                            if (e.key === 'Enter') {
-                                                e.preventDefault();
-                                                submitQuestion();
-                                            }
-                                        }}
-                                        style={{
-                                            borderRadius: '8px',
-                                        }}
-                                    />
-                                    <Form.Control.Feedback type="invalid">
-                                        Please enter at least 25 characters.
-                                    </Form.Control.Feedback>
-                                </Form.Group>
+                            <Form.Group controlId="questionInput">
+                                <Form.Control
+                                    className="ask-input"
+                                    type="text"
+                                    value={inputVal}
+                                    onChange={(e) => {
+                                        setInputVal(e.target.value);
+                                        if (showError && e.target.value.length >= 25) {
+                                            setShowError(false);
+                                        }
+                                    }}
+                                    placeholder={`Ask about ${props.applicant_name_first}’s relevance to the role...`}
+                                    isInvalid={showError}
+                                    maxLength={1000}
+                                    onKeyDown={(e) => {
+                                        if (e.key === 'Enter') {
+                                            e.preventDefault();
+                                            submitQuestion();
+                                        }
+                                    }}
+                                    style={{
+                                        borderRadius: '8px',
+                                    }}
+                                />
+                                <Form.Control.Feedback type="invalid">
+                                    {`Please enter at least ${inputMinLength} characters.`}
+                                </Form.Control.Feedback>
+                            </Form.Group>
                         </Col>
                     </Row>
                 </>
                 : <CenteredSpinner />}
-            <Modal size="lg" show={show} onHide={() => { 
-                                                    setShow(false);
-                                                    setInputVal('');
-                                                    setAnswer('');
-                                                }} 
-                                                centered>
+
+            <Modal size="lg" show={show} onHide={() => {
+                setShow(false);
+                setInputVal('');
+                setAnswer('');
+            }}
+                centered>
                 <div className="card-carousel">
                     <Modal.Header closeButton className="card-section-title">
                         <Modal.Title>Applicant Insights</Modal.Title>
@@ -111,6 +121,7 @@ export const SearchComponent = (props: SearchCompProps) => {
                     </Modal.Footer>
                 </div>
             </Modal>
+
         </Container>
     )
 }
